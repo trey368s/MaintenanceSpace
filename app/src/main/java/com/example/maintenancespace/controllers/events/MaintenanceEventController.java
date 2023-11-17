2c1f8ef8e169278c5f7580c12121226c61f944bf
Capy <cgrothaus@outlook.com>
2023-11-16 21:26:50 -0500

    Add event list

--- app/src/main/java/com/example/maintenancespace/controllers/events/MaintenanceEventController.java
+++ app/src/main/java/com/example/maintenancespace/controllers/events/MaintenanceEventController.java
@@ -1,36 +1,144 @@
 package com.example.maintenancespace.controllers.events;
 
+import android.util.Log;
+
 import com.example.maintenancespace.models.events.MaintenanceEventModel;
 import com.example.maintenancespace.models.events.MaintenanceEventStatus;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.SetOptions;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MaintenanceEventController {
-    public static MaintenanceEventModel fetchById(int maintenanceEventId) {
-        return new MaintenanceEventModel(maintenanceEventId, 2, "Oil Change", "Change oil", 0f, LocalDateTime.now(), MaintenanceEventStatus.FUTURE, 0);
+    private static FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    public interface MaintenanceEventListener {
+        void onEventFetched(MaintenanceEventModel event);
+        void onEventsFetched(ArrayList<MaintenanceEventModel> events);
+        void onCreation(String docId);
+        void onDelete(String docId);
+        void onUpdate(String docId);
+        void onFailure(Exception e);
+    }
+    public static void fetchById(String carId, String maintenanceEventId, MaintenanceEventListener listener) {
+        firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .document(maintenanceEventId)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        MaintenanceEventModel event = documentSnapshot.toObject(MaintenanceEventModel.class);
+                        listener.onEventFetched(event);
+                    } else {
+                        listener.onFailure(new Exception("Event not found"));
+                    }
+                })
+                .addOnFailureListener(e -> listener.onFailure(e));
     }
 
-    public static ArrayList<MaintenanceEventModel> fetchAllByCarId(int carId) {
-        MaintenanceEventModel event = new MaintenanceEventModel(1, carId, "Oil Change", "Change oil", 0f, LocalDateTime.now(), MaintenanceEventStatus.FUTURE, 0);
-        ArrayList<MaintenanceEventModel> events = new ArrayList<>();
-        events.add(event);
-        return events;
+    public static void fetchAllByCarId(String carId, MaintenanceEventListener listener) {
+        firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    ArrayList<MaintenanceEventModel> events = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        MaintenanceEventModel event = document.toObject(MaintenanceEventModel.class);
+                        events.add(event);
+                    }
+                    listener.onEventsFetched(events);
+                })
+                .addOnFailureListener(e -> listener.onFailure(e));
     }
 
-    public static boolean deleteById(int maintenanceEventId) {
-        return true;
+    public static void fetchAllByUserId(String userId, MaintenanceEventListener listener) {
+        firestore.collection("Car")
+                .whereArrayContains("userIds", userId)
+                .get()
+                .addOnSuccessListener(task -> {
+                    for(DocumentSnapshot carDocument : task.getDocuments()) {
+                        carDocument.getReference()
+                                .collection("MaintenanceEvent")
+                                .get()
+                                .addOnSuccessListener(documentSnapshots -> {
+                                    ArrayList<MaintenanceEventModel> events = new ArrayList<>();
+                                    for(DocumentSnapshot eventDocument : documentSnapshots.getDocuments()) {
+                                        MaintenanceEventModel event = eventDocument.toObject(MaintenanceEventModel.class);
+                                        events.add(event);
+                                    }
+                                    listener.onEventsFetched(events);
+                                })
+                                .addOnFailureListener(e -> listener.onFailure(e));
+                    }
+                })
+                .addOnFailureListener(e -> listener.onFailure(e));
     }
 
-    public static MaintenanceEventModel createByCarId(int carId, String name, String description, Float mileage, LocalDateTime date, MaintenanceEventStatus status) {
-        return new MaintenanceEventModel(1, carId, name, description, mileage, date, status);
+    public static void deleteById(String carId, String maintenanceEventId, MaintenanceEventListener listener) {
+        firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .document(maintenanceEventId)
+                .delete()
+                .addOnSuccessListener(aVoid -> {
+                    listener.onDelete(maintenanceEventId);
+                })
+                .addOnFailureListener(e -> {
+                    listener.onFailure(e);
+                });
     }
 
-    public static MaintenanceEventModel updateById(int maintenanceEventId, int carId, String name, String description, Float mileage, LocalDateTime date, MaintenanceEventStatus status) {
-        return new MaintenanceEventModel(maintenanceEventId, carId, name, description, mileage, date, status);
+    public static void createByCarId(String carId, String name, String description, Float mileage, Timestamp date, MaintenanceEventStatus status, MaintenanceEventListener listener) {
+        MaintenanceEventModel event = new MaintenanceEventModel(carId, name, description, mileage, date, status);
+        firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .add(event)
+                .addOnSuccessListener(documentReference -> {
+                    String docId = documentReference.getId();
+                    listener.onCreation(docId);
+                })
+                .addOnFailureListener(e -> listener.onFailure(e));
     }
 
-    public static boolean deleteAllByCarId(int carId) {
-        return true;
+    public static void updateById(String carId, String maintenanceEventId, MaintenanceEventModel updatedEvent, MaintenanceEventListener listener) {
+        Map<String, Object> updatedData = new HashMap<>();
+        updatedData.put("name", updatedEvent.getName());
+        updatedData.put("description", updatedEvent.getDescription());
+        updatedData.put("mileage", updatedEvent.getMileage());
+        updatedData.put("date", updatedEvent.getDate());
+        updatedData.put("status", updatedEvent.getStatus());
+
+        firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .document(maintenanceEventId)
+                .set(updatedData, SetOptions.merge())
+                .addOnSuccessListener(aVoid -> {
+                    listener.onUpdate(carId);
+                })
+                .addOnFailureListener(e -> {
+                    listener.onFailure(e);
+                });
+    }
+
+    public static void deleteAllByCarId(String carId) {
+        List<DocumentSnapshot> events = firestore.collection("Car")
+                .document(carId)
+                .collection("MaintenanceEvent")
+                .get()
+                .getResult()
+                .getDocuments();
+        for(DocumentSnapshot event : events) {
+            event.getReference().delete();
+        }
+
     }
 }
