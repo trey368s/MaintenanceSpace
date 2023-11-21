c23bdcb1e91ae6c01ebf9630d469fdd619ac7199
Gabriel Phan <phanga@mail.uc.edu>
2023-11-21 18:39:55 -0500

    Adding Firebase Auth

---
+++ app/src/main/java/com/example/maintenancespace/controllers/users/UserController.java
@@ -0,0 +1,12 @@
+package com.example.maintenancespace.controllers.users;
+
+public class UserController
+{
+
+    public static int fetchById (int userId){
+        return userId;
+    }
+    public static String fetchByEmail(String email){
+        return email;
+    }
+}
