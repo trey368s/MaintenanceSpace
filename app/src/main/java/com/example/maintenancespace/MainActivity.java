0aaf47169bd96aaf1cdb8eea815880b22ba423ba
treystegeman <trey363636@gmail.com>
2024-01-19 19:17:38 -0500

    simple notifications- rebase

--- app/src/main/java/com/example/maintenancespace/MainActivity.java
+++ app/src/main/java/com/example/maintenancespace/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.maintenancespace;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.content.Context;
 import android.util.Log;
 
 import com.example.maintenancespace.utilities.CsvWriter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.maintenancespace.databinding.ActivityMainBinding;
 
 import java.io.BufferedReader;
@@ -29,8 +38,12 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding binding;
     public static AppCompatActivity activity;
+    private static final String CHANNEL_ID = "UpdateChannel";
+    private static final int NOTIFICATION_ID = 1;
+    private NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +62,71 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel();
+
+        checkAndRequestNotificationPermission(getApplicationContext());
+        showNotification();
+    }
+
+    public void showNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+        Notification.Builder builder = null;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            builder = new Notification.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                    .setContentTitle("Service Update - MaintenanceSpace")
+                    .setContentText("Your [car] is approaching the designated mileage for a [service event].")
+                    .setPriority(Notification.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+        }
+
+        Notification notification;
+        notification = builder.build();
+        notificationManager.notify(NOTIFICATION_ID,notification);
+    }
+
+    public void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            CharSequence channelName = "Maintenance Updates";
+            String channelDescription = "Notification channel for updates about upcoming service events.";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            channel.setDescription(channelDescription);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    public void checkAndRequestNotificationPermission(Context context) {
+        boolean areNotificationsEnabled = NotificationManagerCompat.from(context).areNotificationsEnabled();
+
+        if (!areNotificationsEnabled) {
+            Intent intent = new Intent();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+                intent.putExtra("app_package", getPackageName());
+                intent.putExtra("app_uid", getApplicationInfo().uid);
+            } else {
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+            }
+            startActivity(intent);
+        }
     }
 
     @Override
